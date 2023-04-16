da9d1e39d59558990adcca6cc51c32399bab5ccf
Ryu-nakay <70783066+Ryu-nakay@users.noreply.github.com>
2023-04-16 16:50:12 +0900

    Initial Commit

---
+++ ChatworkMessagingButtonApp/ChatworkMessagingButtonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatworkMessagingButtonAppApp.swift
+//  ChatworkMessagingButtonApp
+//
+//  Created by 中山龍 on 2023/04/16.
+//
+
+import SwiftUI
+
+@main
+struct ChatworkMessagingButtonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
