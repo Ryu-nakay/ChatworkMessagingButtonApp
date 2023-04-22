d27112637562a0e3a4cf16122ee8cfa1762d430c
Ryu-nakay <70783066+Ryu-nakay@users.noreply.github.com>
2023-04-22 12:51:25 +0900

    ChatworkAPITokenLocalRepositoryの作成と実装

---
+++ ChatworkMessagingButtonApp/Repository/ChatworkAPITokenLocalRepository.swift
@@ -0,0 +1,20 @@
+//
+//  ChatworkAPITokenLocalRepository.swift
+//  ChatworkMessagingButtonApp
+//
+//  Created by 中山龍 on 2023/04/22.
+//
+
+import Foundation
+
+struct ChatworkAPITokenLocalRepository {
+    let ChatworkAPITokenUserDefaultKey = "ChatworkAPIToken"
+
+    func save(token: String) {
+        UserDefaults.standard.set(token, forKey: ChatworkAPITokenUserDefaultKey)
+    }
+
+    func load() -> String? {
+        return UserDefaults.standard.string(forKey: ChatworkAPITokenUserDefaultKey)
+    }
+}
